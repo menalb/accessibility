61df825c8ab52809a7fb6529fb7725543240d08e
Alberto Meneghini <menalb84@gmail.com>
2022-02-06 17:23:24 +0100

    category selector

--- blazor/MDStore/src/MDStore/Program.cs
+++ blazor/MDStore/src/MDStore/Program.cs
@@ -8,5 +8,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<RecordService>();
+builder.Services.AddScoped<RecordsStoreState>();
 
 await builder.Build().RunAsync();
