68c96afcd1657e329c3450ff78054c49423728ff
Alberto Meneghini <menalb84@gmail.com>
2022-04-29 17:54:45 +0200

    wirking

---
+++ blazor/BlazorWAI/src/BlazorWAI.WASM/wwwroot/js/validation.js
@@ -0,0 +1,16 @@
+﻿export function ScrollToFirstInvalidElement(form) {
+
+    var field = form.getElementsByClassName('invalid');
+
+    if (!field) {
+        console.warn('element was not found', form);
+    }
+    else if (field.length > 0 && field[0]) {
+        field[0].scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest"
+        });
+        field[0].focus();
+    }
+}
